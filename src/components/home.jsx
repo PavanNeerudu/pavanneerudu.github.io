ebc7444bfe04639f87e7a217144c83b59d29d650
Pavan Neerudu <pavanneerudu7@gmail.com>
2023-09-01 21:05:20 +0530

    Update Home.jsx

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -13,14 +13,14 @@ function Home() {
                     <div class="box">
                         <h1>Hey, I'm Pavan</h1>
                         <hr align="center" />
-                        <p>I am a Computer Sciences undergraduate student graduating in Summer of 2023 from National Institute Of Technology, Warangal.  My academic background involves hands-on experience in computer sciences courses ranging from Web Development, Database Management to Data Science, and Computer Networks.</p>
+                        <p>Welcome to my personal website. I am a Software Engineer at Microsoft and a 2023 Computer Sciences graduate student from National Institute Of Technology, Warangal.  My academic background involves hands-on experience in computer sciences courses ranging from Web Development, Database Management to Data Science, and Computer Networks.</p>
                     </div>
                 </div>
             </div>
             <div class="row">
                 <div class="col-xs-12 box2" id="linktohome">
-                        <p>I am an enthusisatic and disciplined soul. I am always keen to up-skill myself by learning new things whenever I get a chance. During my internship with Azure Kubernetes Services at Microsoft, I learnt about kubernetes, added GPU plugins for Azure Kubernetes Services on Azure Stack HCI, and gained valuabe industry knowledge.</p>
-                        <p>My strengths are my analytical approach, my appreciable communication, and presentation skills. I believe in being realistic. In my free time, I love exploring new technologies, reading novels and spending time with friends. </p>
+                        <p>I am keen to up-skill myself by learning new things whenever I get a chance. During my internship with Hybrid Azure Kubernetes Services at Microsoft, I learnt about kubernetes, added GPU plugins for Azure Kubernetes Services on Azure Stack HCI, and gained valuabe industry knowledge.</p>
+                        <p>Currently, I am working as a Software Engineer in the same team.</p>
                     
                 </div>
             </div>
